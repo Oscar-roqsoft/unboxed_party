452cf7cce00b5005cff3f1eee244c5a1fb73311e
Oscar-roqsoft <oscar.nnoje@gmail.com>
2024-03-09 10:24:56 +0100

    nice

--- pages/admin_slym/items/index.vue
+++ pages/admin_slym/items/index.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
       <v-container>
-        <Backbutton class="" />
-      </v-container>
+            <div  class="  mx-auto">
+
+              <v-btn color="blue-darken-4" @click.prevent="navigateTo('/admin_slym')" class="rounded-xl">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path fill="currentColor" d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64"/>
+                  </svg>
+              </v-btn>
+            </div>
+        </v-container>
 
      
   
--- pages/admin_slym/user/index.vue
+++ pages/admin_slym/user/index.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        <Backbutton class="ma-4"/>
+
+        <div class="ma-4">
+
+          <v-btn color="blue-darken-4" @click.prevent="navigateTo('/admin_slym')" class="rounded-xl">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64"/>
+              </svg>
+          </v-btn>
+        </div>
   
       <v-card tile theme="dark" >
 
